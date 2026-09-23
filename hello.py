4b569c791987a6a7c6148da21a240109105aa7c5
JacekNicz <jacekniczyperowicz01gmail.com>
2026-09-23 22:38:15 +0200

    Dodaj ostrzeżenie przed progiem alertu

--- hello.py
+++ hello.py
@@ -1,5 +1,7 @@
 failed_logins = int(input("Podaj liczbę nieudanych logowań: "))
 if failed_logins >= 5:
     print("ALERT: wysoka liczba nieudanych logowań")
+elif failed_logins == 4:
+    print("OSTRZEŻENIE: zbliżasz się do progu alertu")
 else:
     print("brak alertu")
