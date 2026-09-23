37881e67009c0ebd13bb3c1bfb731d9c1b6d22f0
JacekNicz <jacekniczyperowicz01gmail.com>
2026-09-23 22:13:16 +0200

    Dodaj warunek alertu logowania

--- hello.py
+++ hello.py
@@ -1,2 +1,5 @@
-name = 'Jaca'
-print('witaj, ' + name + '!')
+failed_logins = int(input("Podaj liczbę nieudanych logowań: "))
+if failed_logins >= 5:
+    print("ALERT: wysoka liczba nieudanych logowań")
+else:
+    print("brak alertu")
