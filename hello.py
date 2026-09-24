a45343ff957020e4968a5cfee647f34367b2e08c
JacekNicz <jacekniczyperowicz01gmail.com>
2026-09-24 21:16:06 +0200

    Dodaj analizę udanych i podejrzanych logowań

--- hello.py
+++ hello.py
@@ -1,11 +1,18 @@
-logowania = [1, 0, 1, 1, 0, 1]
+logowania = [1, 1, 1, 1, 1]
 failed_logins = 0
+successful_logins = 0
+podejrzane_logowania = []
 
 for wynik in logowania:
     if wynik == 1:
         failed_logins = failed_logins + 1
-
+        podejrzane_logowania.append(wynik)
+    else:
+        successful_logins = successful_logins + 1  
+        
+print("Podejrzane logowania:", podejrzane_logowania)
 print("Nieudane logowania:", failed_logins)
+print("Udane logowania:", successful_logins)
 if failed_logins >= 5:
     print("ALERT: wysoka liczba nieudanych logowań")
 elif failed_logins == 4:
