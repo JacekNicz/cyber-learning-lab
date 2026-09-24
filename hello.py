0fd4ac972cd458c965f025bf5e5961fed7b1d337
JacekNicz <jacekniczyperowicz01gmail.com>
2026-09-24 21:29:11 +0200

    Zastąp liczniki listami logowań

--- hello.py
+++ hello.py
@@ -1,21 +1,20 @@
 logowania = [1, 0, 1, 1, 0, 1]
-failed_logins = 0
-successful_logins = 0
+successful_logins = []
 podejrzane_logowania = []
 
 for wynik in logowania:
     if wynik == 1:
-        failed_logins = failed_logins + 1
         podejrzane_logowania.append(wynik)
     else:
-        successful_logins = successful_logins + 1  
-        
+        successful_logins.append(wynik)
+
 print("Podejrzane logowania:", podejrzane_logowania)
-print("Nieudane logowania:", failed_logins)
-print("Udane logowania:", successful_logins)
-if failed_logins >= 5:
+print("Liczba elementów na liście:", len(podejrzane_logowania))
+print("Nieudane logowania:", len(podejrzane_logowania))
+print("Udane logowania:", len(successful_logins))
+if len(podejrzane_logowania) >= 5:
     print("ALERT: wysoka liczba nieudanych logowań")
-elif failed_logins == 4:
+elif len(podejrzane_logowania) == 4:
     print("OSTRZEŻENIE: zbliżasz się do progu alertu")
 else:
     print("brak alertu")
