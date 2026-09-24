deadb82634e27470c549140274f724ba0766334a
JacekNicz <jacekniczyperowicz01gmail.com>
2026-09-24 17:36:36 +0200

    Dodaj listę logowań i pętlę for

--- hello.py
+++ hello.py
@@ -1,4 +1,11 @@
-failed_logins = int(input("Podaj liczbę nieudanych logowań: "))
+logowania = [0, 0, 1, 1, 0, 1]
+failed_logins = 0
+
+for wynik in logowania:
+    if wynik == 1:
+        failed_logins = failed_logins + 1
+
+print("Nieudane logowania:", failed_logins)
 if failed_logins >= 5:
     print("ALERT: wysoka liczba nieudanych logowań")
 elif failed_logins == 4:
